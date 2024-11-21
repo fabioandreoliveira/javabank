9eca605db54fe4b3d6492e725f9ffb04dfc6c154
lopes-mafalda <lopes.mafalda97@gmail.com>
2024-11-21 11:47:49 +0000

    adds step-07-jpa

--- src/main/java/io/codeforall/bootcamp/javabank/model/account/AccountType.java
+++ src/main/java/io/codeforall/bootcamp/javabank/model/account/AccountType.java
@@ -1,17 +1,9 @@
 package io.codeforall.bootcamp.javabank.model.account;
 
-/**
- * The possible {@link Account} types
- */
+
 public enum AccountType {
 
-    /**
-     * @see CheckingAccount
-     */
     CHECKING,
-
-    /**
-     * @see SavingsAccount
-     */
     SAVINGS
+
 }
